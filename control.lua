0f4a5a5c9eb71297a33d80cb278bb7385a49bfad
Francois Campbell <campbell.francois@gmail.com>
2020-05-21 18:37:38 -0400

    use sprite buttons; add deletion ability, release 0.0.2

--- control.lua
+++ control.lua
@@ -14,12 +14,10 @@ function save_logistic_layout(player, name)
         end
     end
 
-    global.layouts["restore_logistics_layout:" .. name] = {
-        name = name,
+    global.layouts[name] = {
         slots = slots,
         slot_count = player.character_logistic_slot_count,
     }
-    repaint_frame(player)
 end
 
 function clear_logistic_layout(player)
@@ -28,12 +26,17 @@ function clear_logistic_layout(player)
     end
 end
 
-function restore_logistic_layout(player, layout)
+function restore_logistic_layout(player, name)
     clear_logistic_layout(player)
+    local layout = global.layouts[name]
     player.character_logistic_slot_count = layout.slot_count
     for index, slot in pairs(layout.slots) do
         player.set_personal_logistic_slot(index, slot)
     end
+end
+
+function delete_logistic_layout(player, name)
+    global.layouts[name] = nil
 end
 
 function create_button(player)
@@ -42,12 +45,29 @@ function create_button(player)
         return
     end
 
-    mod_gui.get_button_flow(player).add{
-        type = "button",
-        name = "toggle_saved_logistics_layouts",
-        caption = "Saved Logistics Layouts",
-        style = mod_gui.button_style,
-    }
+    local flow = mod_gui.get_button_flow(player)
+    local button = flow.toggle_saved_logistics_layouts
+    if button then return end
+
+    logistic_robot = game.item_prototypes['logistic-robot']
+    if logistic_robot then
+        flow.add{
+            type = "sprite-button",
+            name = "toggle_saved_logistics_layouts",
+            sprite = "item/logistic-robot",
+            style = mod_gui.button_style,
+            tooltip = "Toggle saved logistics layouts frame",
+            number = 3,
+        }
+    else
+        flow.add{
+            type = "button",
+            name = "toggle_saved_logistics_layouts",
+            caption = "Saved Logistics Layouts",
+            style = mod_gui.button_style,
+            tooltip = "Toggle saved logistics layouts frame",
+        }
+    end
 end
 
 function toggle_frame(player)
@@ -66,7 +86,6 @@ function toggle_frame(player)
         style = mod_gui.frame_style,
         direction = "vertical",
     }
-    repaint_frame(frame)
 end
 
 function repaint_frame(player)
@@ -76,16 +95,36 @@ function repaint_frame(player)
     if not frame then return end
 
     frame.clear()
-    for restore_button_name, layout in pairs(global.layouts) do
-        frame.add{
-            type = "button",
-            caption = layout.name,
-            name = restore_button_name,
+    local layout_table = frame.add{
+        type = "table",
+        column_count = 3,
+    }
+    for layout_name, layout in pairs(global.layouts) do
+        layout_table.add{
+            type = "sprite-button",
+            sprite = "utility/export_slot",
+            tooltip = "Restore saved layout",
+            name = "restore_saved_layout/" .. layout_name,
+        }
+        layout_table.add{
+            type = "sprite-button",
+            sprite = "utility/trash",
+            tooltip = "Delete saved layout",
+            name = "delete_saved_layout/" .. layout_name,
+        }
+        layout_table.add{
+            type = "label",
+            caption = layout_name,
         }
     end
     frame.add{
         type = "line",
         direction = "horizontal",
+    }
+    frame.add{
+        type = "textfield",
+        name = "new_layout_name",
+        tooltip = "The name for this new saved layout"
     }
     frame.add{
         type = "button",
@@ -100,19 +139,28 @@ function repaint_frame(player)
 end
 
 function on_button_click(event)
-    local name = event.element.name
     local player = game.players[event.player_index]
-    local saved_layout_to_restore = global.layouts[name]
+
+    local name = event.element.name
+    local _, _, restore_layout_name = string.find(name, "restore_saved_layout/(%a+)")
+    local _, _, delete_layout_name = string.find(name, "delete_saved_layout/(%a+)")
 
     if name == "toggle_saved_logistics_layouts" then
         toggle_frame(player)
     elseif name == "save_current_logistics_layout" then
-        save_logistic_layout(player, "franktest")
+        local new_layout_name = mod_gui.get_frame_flow(player).saved_logistics_frame.new_layout_name.text
+        save_logistic_layout(player, new_layout_name)
     elseif name == "clear_current_logistics_layout" then
         clear_logistic_layout(player)
-    elseif saved_layout_to_restore then
-        restore_logistic_layout(player, saved_layout_to_restore)
+    elseif restore_layout_name then
+        restore_logistic_layout(player, restore_layout_name)
+    elseif delete_layout_name then
+        delete_logistic_layout(player, delete_layout_name)
+    else
+        return
     end
+
+    repaint_frame(player)
 end
 
 function setup()
@@ -126,5 +174,6 @@ script.on_init(setup)
 script.on_event(defines.events.on_gui_click, on_button_click)
 
 remote.add_interface("bluegistics", {
-    clear_globals=function() global.layouts = {}; repaint_frame(game.player) end
+    clear_globals=function() global.layouts = {}; repaint_frame(game.player) end,
+    reinit=setup,
 })
